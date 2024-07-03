b0c6687aa31215e3b2ccf303f06936a2be825f94
Imaad Schuller <imaadschullerr@gmail.com>
2024-07-03 12:39:49 +0200

    Edit print statements

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,6 +1,6 @@
 def display_menu():
-    print("\nShopping List Manager")
-    print("1. Add an item")
+    print("Shopping List Manager")
+    print("1. Add Item")
     print("2. Remove an item")
     print("3. View the list")
     print("4. Exit")
